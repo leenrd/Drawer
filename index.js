0c60c58821b04b65f2c75227f58856443e1774d0
Leenard <lolleenard0@gmail.com>
2024-06-11 21:45:03 +0800

    feat: upload * 2

--- index.js
+++ index.js
@@ -1,23 +1,54 @@
-let express = require("express");
-let app = express();
-let host = 3000;
-let server = app.listen(host);
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+const path = require("path");
 
-app.use(express.static("public"));
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
 
-console.log("Socket running. localhost:" + host);
+const port = process.env.PORT || 3000;
 
-let socket = require("socket.io");
-let io = socket(server);
+let rooms = [];
 
-io.sockets.on("connection", newConnection);
+app.use(express.static(path.join(__dirname, "public")));
 
-function newConnection(socket) {
-  console.log("connection:", socket.id);
-  socket.on("mouse", mouseMsg);
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "rooms.html"));
+});
 
-  function mouseMsg(data) {
-    socket.broadcast.emit("mouse", data);
-    console.log(data);
-  }
-}
+app.get("/canvas.html", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "canvas.html"));
+});
+
+io.on("connection", (socket) => {
+  console.log("A user connected");
+
+  socket.on("createRoom", (room) => {
+    if (!rooms.includes(room)) {
+      rooms.push(room);
+    }
+  });
+
+  socket.on("getRooms", () => {
+    socket.emit("availableRooms", rooms);
+  });
+
+  socket.on("joinRoom", (room) => {
+    socket.join(room);
+    socket.room = room;
+    console.log(`User joined room: ${room}`);
+  });
+
+  socket.on("drawing", (data) => {
+    socket.to(socket.room).emit("drawing", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("A user disconnected");
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
